2d2826efe0706b95285cd05667d1c33ddc41fa79
Prithvi Singh <prithvi.chandla@gmail.com>
2024-09-20 11:31:01 +0530

    Added shortcut for closing all buffers except the last one.

--- lua/prithvisingh18/remap.lua
+++ lua/prithvisingh18/remap.lua
@@ -3,6 +3,10 @@ vim.keymap.set("n", "<leader>ls", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>wn", vim.cmd.bnext)
 vim.keymap.set("n", "<leader>wp", vim.cmd.bprevious)
 vim.keymap.set("n", "<leader>ln", ":set number<CR>")
+vim.keymap.set("n", "<leader>bca", ":%bd|e#<CR>")
+
+
+
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
